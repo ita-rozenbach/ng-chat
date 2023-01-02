155f599153ae0fd38a03f1ddc924a8321789e43c
unknown <8667572@gmail.com>
2023-01-03 00:05:35 +0200

    first commit - open project

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const moduls:any = [];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, moduls
+  ],
+  exports: moduls
+})
+export class MaterialModule { }
